ad1ceb60f57f459273e65a5674f06983a2c9af06
Riko <riko.fluchel@gmail.com>
2025-02-17 10:19:20 -0500

    complete advanced deliverables

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 
 //Callbacks
 //Core Deliverables
+
 const handleClick = (ramen) => {
 	//get the ramenDetail div
 	const ramenDetail = document.getElementById('ramen-detail');
 
 	//set the ramenDetail elements with the clicked ramen details
 	ramenDetail.querySelector(`.detail-image`).src = ramen.image;
+	ramenDetail.querySelector(`.detail-image`).id = ramen.id;
 	ramenDetail.querySelector(`.name`).textContent = ramen.name;
 	ramenDetail.querySelector(`.restaurant`).textContent = ramen.restaurant;
 	document.getElementById(`rating-display`).textContent = ramen.rating;
@@ -15,29 +17,32 @@ const handleClick = (ramen) => {
 };
 
 const addSubmitListener = () => {
-	const form = document.getElementById("new-ramen");
-	const ramenMenu = document.getElementById("ramen-menu");
+	const addForm = document.getElementById("new-ramen");
 
-	form.addEventListener("submit", (e) => {
+	addForm.addEventListener("submit", (e) => {
 		e.preventDefault();
 
-		//create ramen object with input values from form so that it can be passed to handleClick
+		//create ramen request object to send post request
 
 		const newRamen = {
-			name: e.target["new-name"].value,
-			image: e.target["new-image"].value,
-			restaurant: e.target["new-restaurant"].value,
-			rating: e.target["new-rating"].value,	
-			comment: e.target["new-comment"].value
-		};
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+			body: JSON.stringify({
+				name: e.target["new-name"].value,
+				image: e.target["new-image"].value,
+				restaurant: e.target["new-restaurant"].value,
+				rating: e.target["new-rating"].value,	
+				comment: e.target["new-comment"].value
+			})};
 
-		let image = document.createElement("img")
-		image.src = newRamen.image;
-		image.addEventListener('click', () => handleClick(newRamen));
+		fetch("http://localhost:3000/ramens", newRamen)
+			.then(res => res.json())
+			.then(data => displayRamens())
 
-		ramenMenu.append(image)
-
-		form.reset();
+		addForm.reset();
 
 	})
 }
@@ -51,7 +56,12 @@ const displayRamens = () => {
 		json.forEach((ramen) => {
 			let image = document.createElement("img");
 			image.src = ramen.image;
-			ramenMenu.append(image)
+			image.id = ramen.id;
+			//Load the first ramen in ramen detail using handleClick
+			if (ramen.id === "1"){
+				handleClick(ramen)
+			}
+			ramenMenu.append(image);
 			image.addEventListener("click", () => handleClick(ramen))
 		})
 	})
@@ -63,11 +73,69 @@ const main = () => {
 	document.addEventListener("DOMContentLoaded", () => {
 		displayRamens();
 		addSubmitListener();
+		editSubmitListener();
+		deleteRamen();
 	})
-
 }
 
 //Advanced Deliverables
+const editSubmitListener = () => {
+	const editForm = document.getElementById("edit-ramen");
+
+	editForm.addEventListener("submit", (e) => {
+		e.preventDefault();
+		//get the Ramen's ID from image in Ramen Detail Div
+		const ramenDetail = document.getElementById('ramen-detail');
+		const ramenImage = ramenDetail.querySelector("img")
+		const ramenId = ramenImage.id
+
+		//create edited ramen patch request
+		const editedRamen = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+			body: JSON.stringify({
+				id: ramenId,
+				rating: e.target["edit-rating"].value,
+				comment: e.target["edit-comment"].value
+			})
+
+		};
+		fetch(`http://localhost:3000/ramens/${ramenId}`, editedRamen)
+			.then(res => res.json())
+			.then(data => displayRamens())
+		editForm.reset()
+	})
+}
+
+const deleteRamen = () => {
+	const deleteButton = document.getElementById("delete");
+
+	deleteButton.addEventListener("click", () => {
+		//get the Ramen's ID from image in Ramen Detail Div
+		const ramenDetail = document.getElementById('ramen-detail');
+		const ramenImage = ramenDetail.querySelector("img")
+		const ramenId = ramenImage.id
+
+		//create delete request object to send
+		const deleteRequest = {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				"Accept": "application/json"
+			},
+			body: JSON.stringify({
+				id: ramenId
+			})
+		}
+
+		fetch(`http://localhost:3000/ramens/${ramenId}`, deleteRequest)
+			.then(res => res.json())
+			.then(data => displayRamens())
+	})
+}
 
 
 main()
